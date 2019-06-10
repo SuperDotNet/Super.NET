ea66b5f0416eff47f1a5fbcab794bbdd3733982f
Mike-EEE <michael@dragonspark.network>
2019-06-10 12:24:52 -0400

    Basic sketching around XML writer. #15

---
+++ Super.Serialization.Testing.Application/ElementWriterTests.cs
@@ -0,0 +1,66 @@
+﻿using BenchmarkDotNet.Attributes;
+using FluentAssertions;
+using Super.Model.Sequences;
+using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace Super.Serialization.Testing.Application
+{
+	public sealed class ElementWriterTests
+	{
+		[Fact]
+		void Verify()
+		{
+			Subject.Default.Get(12345)
+			       .Open()
+			       .To(Encoding.UTF8.GetString)
+			       .Should()
+			       .Be($@"<?xml version=""1.0""?>{Environment.NewLine}<unsignedInt>12345</unsignedInt>");
+		}
+
+		[Fact]
+		void VerifyClassic()
+		{
+			var serializer = new XmlSerializer(typeof(uint));
+
+			using (var stream = new MemoryStream())
+			{
+				serializer.Serialize(stream, 12345u);
+				Subject.Default.Get(12345u).Open().Should().Equal(stream.ToArray());
+			}
+		}
+
+		sealed class Subject : Writer<uint>
+		{
+			public static Subject Default { get; } = new Subject();
+
+			Subject() : base(new XmlDocumentEmitter<uint>(new Emit<uint>(Declaration<uint>.Default),
+			                                              new XmlElementWriter<uint>("unsignedInt", PositiveNumber.Default))) {}
+		}
+
+		public class Benchmarks
+		{
+			readonly XmlSerializer _serializer;
+
+			public Benchmarks() : this(new XmlSerializer(typeof(uint))) {}
+
+			public Benchmarks(XmlSerializer serializer) => _serializer = serializer;
+
+			[Benchmark(Baseline = true)]
+			public byte[] Classic()
+			{
+				using (var stream = new MemoryStream())
+				{
+					_serializer.Serialize(stream, 12345u);
+					return stream.ToArray();
+				}
+			}
+
+			[Benchmark]
+			public Array<byte> Measure() => Subject.Default.Get(12345);
+		}
+	}
+}
--- Super.Serialization/Writer.cs
+++ Super.Serialization/Writer.cs
@@ -5,6 +5,7 @@ using System;
 using System.Buffers;
 using System.Buffers.Text;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Super.Serialization
 {
@@ -24,25 +25,175 @@ namespace Super.Serialization
 		DefaultBufferSize() : base(1024 * 16) {}
 	}
 
+	public interface IAdvance<T> : ISelect<Composition<T>, uint>, IResult<uint> {}
+
+	public interface ICharacter : IResult<byte> {}
+
+	public class Character : Instance<byte>, ICharacter
+	{
+		public Character(char instance) : base((byte)instance) {}
+	}
+
+	public sealed class Open : Character
+	{
+		public static Open Default { get; } = new Open();
+
+		Open() : base('<') {}
+	}
+
+	public sealed class Close : Character
+	{
+		public static Close Default { get; } = new Close();
+
+		Close() : base('>') {}
+	}
+
+	public sealed class Marker : Character
+	{
+		public static Marker Default { get; } = new Marker();
+
+		Marker() : base('/') {}
+	}
+
+	sealed class Declaration<T> : Instance<uint>, IAdvance<T>
+	{
+		public static Declaration<T> Default { get; } = new Declaration<T>();
+
+		Declaration() : this(Encoding.UTF8.GetBytes($@"<?xml version=""1.0""?>{Environment.NewLine}")) {}
+
+		
+		readonly byte[] _content;
+
+		public Declaration(byte[] content) : base((uint)content.Length) => _content = content;
+
+		public uint Get(Composition<T> parameter)
+		{
+			_content.CopyTo(parameter.Output, parameter.Index);
+			return (uint)_content.Length;
+		}
+	}
+
+	sealed class OpenContent<T> : Instance<uint>, IAdvance<T>
+	{
+		readonly byte[] _name;
+		readonly byte   _first, _last;
+
+		public OpenContent(string name) : this(Encoding.UTF8.GetBytes(name), Open.Default, Close.Default) {}
+
+		public OpenContent(byte[] name, byte first, byte last) : base((uint)name.Length + 2u)
+		{
+			_name  = name;
+			_first = first;
+			_last  = last;
+		}
+
+		public uint Get(Composition<T> parameter)
+		{
+			var output = parameter.Output;
+			var result = parameter.Index;
+			output[result++] = _first;
+			_name.CopyTo(output, result);
+			output[result += (uint)_name.Length] = _last;
+			return ++result - parameter.Index;
+		}
+	}
+
+	sealed class CloseContent<T> : Instance<uint>, IAdvance<T>
+	{
+		readonly byte[] _name;
+		readonly byte   _first;
+		readonly byte   _marker;
+		readonly byte   _last;
+
+		// ReSharper disable once TooManyDependencies
+		public CloseContent(string name)
+			: this(Encoding.UTF8.GetBytes(name), Open.Default, Marker.Default, Close.Default) {}
+
+		public CloseContent(byte[] name, byte first, byte marker, byte last) : base((uint)name.Length + 3u)
+		{
+			_name   = name;
+			_first  = first;
+			_marker = marker;
+			_last   = last;
+		}
+
+		public uint Get(Composition<T> parameter)
+		{
+			var output = parameter.Output;
+			var result = parameter.Index;
+			output[result++] = _first;
+			output[result++] = _marker;
+			_name.CopyTo(output, result);
+			output[result += (uint)_name.Length] = _last;
+			return ++result - parameter.Index;
+		}
+	}
+
+	class XmlElementWriter<T> : ElementWriter<T>
+	{
+		public XmlElementWriter(string name, IEmit<T> content)
+			: base(new Emit<T>(new OpenContent<T>(name)), content, new Emit<T>(new CloseContent<T>(name))) {}
+	}
+
+	public class XmlDocumentEmitter<T> : IEmit<T>
+	{
+		readonly IEmit<T> _declaration;
+		readonly IEmit<T> _content;
+
+		public XmlDocumentEmitter(IEmit<T> declaration, IEmit<T> content)
+		{
+			_declaration = declaration;
+			_content     = content;
+		}
+
+		public Model.Sequences.Store<byte> Get(Composition<T> parameter)
+		{
+			var declaration =
+				_declaration.Get(new Composition<T>(parameter.Output, parameter.Instance, parameter.Index));
+			var result = _content.Get(new Composition<T>(declaration.Instance, parameter.Instance, declaration.Length));
+			return result;
+		}
+	}
+
+	class ElementWriter<T> : IEmit<T>
+	{
+		readonly IEmit<T> _start, _content, _finish;
+
+		public ElementWriter(IEmit<T> start, IEmit<T> content, IEmit<T> finish)
+		{
+			_start   = start;
+			_content = content;
+			_finish  = finish;
+		}
+
+		public Model.Sequences.Store<byte> Get(Composition<T> parameter)
+		{
+			var start   = _start.Get(new Composition<T>(parameter.Output, parameter.Instance, parameter.Index));
+			var content = _content.Get(new Composition<T>(start.Instance, parameter.Instance, start.Length));
+			var result  = _finish.Get(new Composition<T>(content.Instance, parameter.Instance, content.Length));
+			return result;
+		}
+	}
+
 	class Writer<T> : IWriter<T>
 	{
-		readonly IEmit<T>        _writer;
+		readonly IEmit<T>        _emitter;
 		readonly ArrayPool<byte> _pool;
 		readonly uint            _size;
 
-		public Writer(IEmit<T> writer) : this(writer, DefaultBufferSize.Default) {}
+		public Writer(IEmit<T> emitter) : this(emitter, DefaultBufferSize.Default) {}
 
-		public Writer(IEmit<T> writer, uint size) : this(writer, ArrayPool<byte>.Shared, size) {}
+		public Writer(IEmit<T> emitter, uint size) : this(emitter, ArrayPool<byte>.Shared, size) {}
 
-		public Writer(IEmit<T> writer, ArrayPool<byte> pool, uint size)
+		public Writer(IEmit<T> emitter, ArrayPool<byte> pool, uint size)
 		{
-			_writer = writer;
-			_pool   = pool;
-			_size   = size;
+			_emitter = emitter;
+			_pool    = pool;
+			_size    = size;
 		}
 
-		public Array<byte> Get(T parameter) => _writer.Get(new Composition<T>(_pool.Rent((int)_size), parameter))
-		                                              .Complete(_pool);
+		public Array<byte> Get(T parameter) => _emitter.Get(new Composition<T>(_pool.Rent((int)_size), parameter))
+		                                               .Complete(_pool);
 	}
 
 	public delegate uint Advance<T>(Composition<T> parameter);
@@ -51,6 +202,8 @@ namespace Super.Serialization
 	{
 		readonly uint       _size;
 		readonly Advance<T> _advance;
+
+		public Emit(IAdvance<T> advance) : this(advance.Get(), advance.Get) {}
 
 		public Emit(uint size, Advance<T> advance)
 		{
@@ -64,11 +217,21 @@ namespace Super.Serialization
 				                  ? new Composition<T>(parameter.Output.Copy(in _size), parameter.Instance,
 				                                       parameter.Index)
 				                  : parameter;
-			return new Model.Sequences.Store<byte>(composition.Output, _advance(composition));
+			return new Model.Sequences.Store<byte>(composition.Output, parameter.Index + _advance(composition));
 		}
 	}
 
 	public interface IEmit<T> : ISelect<Composition<T>, Model.Sequences.Store<byte>> {}
+
+	sealed class EmptyEmit<T> : IEmit<T>
+	{
+		public static EmptyEmit<T> Default { get; } = new EmptyEmit<T>();
+
+		EmptyEmit() {}
+
+		public Model.Sequences.Store<byte> Get(Composition<T> parameter)
+			=> new Model.Sequences.Store<byte>(parameter.Output, parameter.Index);
+	}
 
 	sealed class PositiveNumber : Emit<uint>
 	{
@@ -76,9 +239,9 @@ namespace Super.Serialization
 
 		PositiveNumber()
 			: base(20, x => Utf8Formatter.TryFormat(x.Instance, x.Output.AsSpan((int)x.Index), out var count)
-					              ? (uint)count
-					              : throw new
-						                InvalidOperationException($"Could not format '{x.Instance}' into its UTF8 equivalent.")) {}
+				                ? (uint)count
+				                : throw new
+					                  InvalidOperationException($"Could not format '{x.Instance}' into its UTF8 equivalent.")) {}
 	}
 
 	static class Extensions
